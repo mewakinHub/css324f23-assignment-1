9ceff5a2c7625bc86d696f998545d8736df6a7c7
Mewakin <mewteetawat@gmail.com>
2023-09-10 16:01:19 +0700

    Update members.py
    dont know three name and num

--- task-0/members.py
+++ task-0/members.py
@@ -1,4 +1,4 @@
 [
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
+    {"name": "Teetawat Bussabaratu", "sid": "6422782423"},
+    {"name": "THree": "642277*****"},
 ]
